22b195260fe3b74239388d2fb5952fb30c4a2e56
Sholokhov99 <sholokhov.daniil@gmail.com>
2024-08-17 19:46:47 +0700

    Создан базовый контейнер данных

---
+++ src/Container/Container.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Sholokhov\BitrixModels\Container;
+
+class Container
+{
+    /**
+     * Данные контейнера
+     *
+     * @var array
+     */
+    protected array $data = [];
+
+    /**
+     * Проверка наличия значения по идентификатору
+     *
+     * @param string $id
+     * @return bool
+     */
+    public function has(string $id): bool
+    {
+        return array_key_exists($id, $this->data);
+    }
+
+    /**
+     * Получение значения
+     *
+     * @param string $id
+     * @param mixed|null $default
+     * @return mixed
+     */
+    public function get(string $id, mixed $default = null): mixed
+    {
+        return $this->data[$id] ?? $default;
+    }
+
+    /**
+     * Установка значения
+     *
+     * @param string $id
+     * @param mixed $value
+     * @return $this
+     */
+    public function set(string $id, mixed $value): self
+    {
+        $this->data[$id] = $value;
+        return $this;
+    }
+}
